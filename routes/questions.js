0d81d42d69376e802f0e672fb1061dea75ee4b76
Brynja Pálína Sigurgeirsdóttir <brynjaps@outlook.com>
2018-04-11 16:55:27 +0000

    Update questions.js

--- routes/questions.js
+++ routes/questions.js
@@ -88,7 +88,7 @@ router.route('/questions?score').get(function(req, res) {
   });
 });
 
-router.get('/questions/:score', (req, res) => {
+router.get('/questions/users/:score', (req, res) => {
   const { score } = req.params;
 
   const item = Question.find(i => i.score === score);
